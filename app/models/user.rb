6c1f09a4cb8dfc986bb27801c3ecc90da0a06aee
minhngocln <minhngoc.ln@gmail.com>
2015-09-17 11:34:06 -0700

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  validates :password_digest, presence: true
+  validates :email, :session_token, presence: true, uniqueness: true
+
+  validates :password, allow_nil: true , length: { minimum: 6 }
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return user if user && user.is_password?(password)
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    session_token ||= reset_session_token!
+  end
+
+  def reset_session_token!
+    session_token = SecureRandom.base64
+    self.update(session_token: session_token)
+    session_token
+  end
+end
